954eb1b4c783d2fe2306c306acc8e68dbff78382
Arghyadip Chakraborty <arghyadip.chak@rediffmail.com>
2020-10-31 12:19:41 +0530

    Update

--- tcbbot.py
+++ tcbbot.py
@@ -1,5 +1,8 @@
 import discord, asyncio
 from discord.ext.commands import Bot
+from memelib.api import DankMemeClient
+
+await myclient.meme(subreddit="dankmemes")
 
 def get_token():
     import pickle
@@ -8,10 +11,16 @@ def get_token():
     return None
 
 client = Bot('?')
+myclient = DankMemeClient(return_embed=True)
 
 @client.command(name="hello")
 async def hello(ctx):
     await ctx.send("Hello I'm the TCB Bot! I can't do anything yet, but I will soon.")
+
+@client.command(name="memes")
+async def memes(ctx):
+    memb = await myclient.meme(subreddit="dankmemes")
+    await ctx.send(embed=memb)
 
 @client.event
 async def on_message(message):
