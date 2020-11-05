6fed295a9cd5a17386e6b252f228c6b50ab750aa
Arghyadip Chakraborty <arghyadip.chak@rediffmail.com>
2020-11-05 09:36:09 +0530

    Update

--- crackwatch/local_crack.py
+++ crackwatch/local_crack.py
@@ -1,12 +1,13 @@
 from fetch_crackwatch import *
 from pymongo import MongoClient
 from time import sleep
+from datetime import datetime
+
+time_stamp = str(datetime.now())
 
 client = MongoClient()
 crackwatch = client.crackwatch
 cracked = crackwatch.cracked
-
-time_stamp = ""
 
 fetcher = fetch_cracked_gen()
 while True:
@@ -18,3 +19,16 @@ while True:
             cracked.insert_one(game)
     sleep(1)
 cracked.delete_many({'lastUpdated': {'$ne': time_stamp}})
+
+uncracked = crackwatch.cracked
+
+fetcher = fetch_uncracked_gen()
+while True:
+    games = next(fetcher)
+    if not games: break
+    for game in games:
+        game['lastUpdated'] = time_stamp
+        if not uncracked.update_one({'_id': game['_id']}, {'$set': game}):
+            uncracked.insert_one(game)
+    sleep(1)
+uncracked.delete_many({'lastUpdated': {'$ne': time_stamp}})
