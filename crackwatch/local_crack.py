0442afb164b16f1424f38f093a08cbd4d08dacd4
Arghyadip Chakraborty <arghyadip.chak@rediffmail.com>
2020-11-05 12:51:09 +0530

    Update

--- crackwatch/local_crack.py
+++ crackwatch/local_crack.py
@@ -4,31 +4,37 @@ from time import sleep
 from datetime import datetime
 
 time_stamp = str(datetime.now())
+print("TimeStamp:", time_stamp)
 
 client = MongoClient()
 crackwatch = client.crackwatch
-cracked = crackwatch.cracked
 
+cracked = crackwatch.cracked
 fetcher = fetch_cracked_gen()
+cracked_count = 0
 while True:
     games = next(fetcher)
     if not games: break
+    cracked_count+= len(games)
+    print("Cracked:", cracked_count)
     for game in games:
         game['lastUpdated'] = time_stamp
-        if not cracked.update_one({'_id': game['_id']}, {'$set': game}):
+        if not cracked.update_one({'_id': game['_id']}, {'$set': game}).modified_count:
             cracked.insert_one(game)
     sleep(1)
 cracked.delete_many({'lastUpdated': {'$ne': time_stamp}})
 
 uncracked = crackwatch.cracked
-
 fetcher = fetch_uncracked_gen()
+uncracked_count = 0
 while True:
     games = next(fetcher)
     if not games: break
+    uncracked_count+= len(games)
+    print("Uncracked:", uncracked_count)
     for game in games:
         game['lastUpdated'] = time_stamp
-        if not uncracked.update_one({'_id': game['_id']}, {'$set': game}):
+        if not uncracked.update_one({'_id': game['_id']}, {'$set': game}).modified_count:
             uncracked.insert_one(game)
     sleep(1)
 uncracked.delete_many({'lastUpdated': {'$ne': time_stamp}})
