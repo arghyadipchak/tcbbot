e39a3c051bf08ee9ef51bba497bb1b2ad0f9128c
Arghyadip Chakraborty <arghyadip.chak@rediffmail.com>
2020-11-14 12:28:11 +0530

    Update

--- cogs/crackwatch.py
+++ cogs/crackwatch.py
@@ -1,4 +1,4 @@
-import discord
+import discord, asyncio
 from discord.ext import commands
 import os, re
 from pymongo import MongoClient
@@ -12,22 +12,56 @@ class CrackWatch(commands.Cog):
         self.db_client = MongoClient(username = USER, password = PASSWORD, authSource = DB)
         self.db = self.db_client[DB]
 
-    def find_game(self, gname):
+    def find_games(self, gname):
         gslug = '-'.join(re.findall(r"[a-z0-9]+", gname.lower()))
         games = self.db.allgames
-        return games.find_one({'slug': gslug})
+        return list(games.find({'slug': {'$regex' : f".*{gslug}.*"}})[:10])
+
+    def get_embed(self, game):
+        if game:
+            emb = discord.Embed(title = game['title'])
+            emb.add_field(name = "Crack Status:", value = game['crackStatus'])
+            emb.set_image(url = game['image'])
+            return emb
+        else:
+            return discord.Embed()
 
     @commands.command(name="crack")
     async def crack(self, ctx, *args):
+        print(ctx.message)
         gname = ' '.join(args)
         if args:
-            game = self.find_game(gname)
-            if game:
-                emb = discord.Embed(title = game['title'])
-                emb.add_field(name = "Crack Status:", value = game['crackStatus'])
-                emb.set_image(url = game['image'])
-                await ctx.send(embed = emb)
+            games = self.find_games(gname)
+            if games:
+                c = 0
+                ng = len(games)
+                msg = await ctx.send(embed = self.get_embed(games[c]))
+                if ng==1: return
+
+                await msg.add_reaction("⬅")
+                await msg.add_reaction("➡")
+                await msg.add_reaction("☑")
+
+                def reaction_check(reaction, user):
+                    return user!=self.bot.user and reaction.message==msg and (str(reaction.emoji)=="⬅" or str(reaction.emoji)=="➡" or str(reaction.emoji)=="☑")
+
+                while True:
+                    try:
+                        reaction, user = await self.bot.wait_for('reaction_add', timeout = 120.0, check = reaction_check)
+                    except asyncio.TimeoutError:
+                        break
+                    else:
+                        if str(reaction.emoji)=="⬅":
+                            c = (c-1)%ng
+                        elif str(reaction.emoji)=="➡":
+                            c = (c+1)%ng
+                        else:
+                            break
+                        await msg.remove_reaction(reaction, user)
+                        await msg.edit(embed = self.get_embed(games[c]))
+                await msg.clear_reactions()
+                
             else:
-                await ctx.send(f"{gname}: Not Found!")
+                await ctx.send("Nothing Found!")
         else:
             await ctx.send("Please Enter Game!")
